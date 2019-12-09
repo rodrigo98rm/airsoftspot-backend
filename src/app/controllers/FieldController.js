ccf1495f45166c90ac176b19bd7be5a133c760db
rodrigo98rm <mayerrodrigo98@gmail.com>
2019-12-09 15:08:28 -0300

    Create, update and get field

---
+++ src/app/controllers/FieldController.js
@@ -0,0 +1,91 @@
+import * as Yup from 'yup';
+import pool from '../../config/db';
+
+const ADMIN = 1;
+const MODERATOR = 2;
+
+const schema = Yup.object().shape({
+  name: Yup.string().required(),
+  address: Yup.string().required(),
+  about: Yup.string().required(),
+  site: Yup.string().required(),
+});
+
+class FieldController {
+  async createField(req, res) {
+    // Body validation
+    try {
+      schema.validateSync(req.body);
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+
+    const { name, address, about, site } = req.body;
+
+    try {
+      const resultField = await pool.query(
+        'INSERT INTO field (name, address, about, site) VALUES ($1, $2, $3, $4) RETURNING *;',
+        [name, address, about, site]
+      );
+
+      const field = resultField.rows[0];
+      const fieldId = field.fieldid;
+
+      const resultAdmin = await pool.query(
+        'INSERT INTO admin (userId, fieldId, role) VALUES ($1, $2, $3) RETURNING *',
+        [req.userId, fieldId, ADMIN]
+      );
+      return res.json(resultAdmin.rows);
+    } catch (error) {
+      return res.status(500).send({ error: 'Error' });
+    }
+  }
+
+  async updateField(req, res) {
+    // Body validation
+    try {
+      schema.validateSync(req.body);
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+
+    const { name, address, about, site } = req.body;
+
+    try {
+      // Check if field belongs to the user
+      const adminResult = await pool.query(
+        'SELECT * FROM admin WHERE userId = $1 AND fieldId = $2;',
+        [req.userId, req.params.fieldId]
+      );
+
+      const isAdmin = adminResult.rows[0];
+
+      if (!isAdmin) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const result = await pool.query(
+        'UPDATE field SET name = $2, address = $3, about = $4, site = $5 WHERE fieldId = $1 RETURNING *',
+        [req.params.fieldId, name, address, about, site]
+      );
+
+      return res.json(result.rows);
+    } catch (error) {
+      return res.status(500).send({ error: 'Error' });
+    }
+  }
+
+  async getField(req, res) {
+    try {
+      const result = await pool.query(
+        'SELECT * FROM field WHERE fieldId = $1',
+        [req.params.fieldId]
+      );
+      return res.json(result.rows[0]);
+    } catch (error) {
+      return res.status(500).send({ error: 'Error' });
+    }
+  }
+}
+
+export default new FieldController();
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 // Controllers
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import FieldController from './app/controllers/FieldController';
 
 // Middlewares
 import authMiddleware from './app/middlewares/auth';
@@ -19,10 +20,18 @@ routes.post('/session', SessionController.store);
 // Users
 routes.post('/users', UserController.createUser);
 
+// Fields
+routes.get('/field/:fieldId', FieldController.getField);
+
 // Define auth middleware for all routes below this line
 routes.use(authMiddleware);
 
+// Users
 routes.get('/users', UserController.getUserById);
 routes.put('/users', UserController.updateUser);
+
+// Fields
+routes.post('/field', FieldController.createField);
+routes.put('/field/:fieldId', FieldController.updateField);
 
 export default routes;
